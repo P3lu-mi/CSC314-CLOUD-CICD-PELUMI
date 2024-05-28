0dd4e43942883c67a85a9b0e3344c3e3077525e8
P3lu-mi <pelumi4real85@gmail.com>
2024-05-28 17:25:34 +0100

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port =process.env.port || 8080;
 
 app.use(express.static("public"));
 //app.get('/', (req, res) => res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS AKOLADE OLUWAPELUMI VUG/CSC/21/5168'));
