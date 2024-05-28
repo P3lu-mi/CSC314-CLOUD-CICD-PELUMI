ee7a6f2c7bf98e8436b1fbd61798fc6cd61ba9b2
P3lu-mi <pelumi4real85@gmail.com>
2024-05-28 14:34:22 +0100

    added double quote to public in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 //app.get('/', (req, res) => res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS AKOLADE OLUWAPELUMI VUG/CSC/21/5168'));
 
 app.listen(port);
